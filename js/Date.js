2d22bcdb3ea73b2661bdb12d16ee83057ddc86ac
code-hopper <ameykhondekar01@gmail.com>
2025-01-08 08:55:24 +0530

    exception and date class

--- js/Date.js
+++ js/Date.js
@@ -1,7 +1,20 @@
 // to use the date class i.e. the properties and methods 
 // we have to create a date object
 
+// let date = new Date()
+// using date class contructor
+// yyyy-mm-ddThours:minutes:seconds
+// let date = new Date("2024-12-05T00:00:00")
+
 let date = new Date()
+
+date.setDate(5)
+date.setMonth(5)
+date.setFullYear(2026)
+date.setHours(0)
+date.setMinutes(0)
+date.setSeconds(0)
+date.setMilliseconds(0)
 
 // date.properties & date.methods
 
@@ -12,3 +25,20 @@ console.log(date.getFullYear())
 // starting from 1
 console.log(date.getDay())
 console.log(date.getTime())
+
+// getter & setter (contructor) 
+
+console.log(date.getTimezoneOffset())
+
+console.log(date.toTimeString())
+console.log(date.toLocaleTimeString())
+console.log(date.toDateString())
+console.log(date.toLocaleDateString())
+
+let student = {
+    admitDate : new Date().toDateString()
+}
+
+console.log(student.admitDate)
+
+// use to date class is to give timestamp at a created data for a database
